8c0f7b3278db6ed99fbbe935337335e1d0978c66
KalkulatorRK <lnk@utbniti.ru>
2025-11-05 22:24:16 +0300

    Обновлен vite.config для vercel

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production'
-    ? '/KalkulatorRK2/'
-    : '/', // важно для корректных путей на GitHub Pages
+  plugins: [react()],
+  base: '/', // для Vercel используйте '/'
 })
